16e5570cc0a8d85f6e1cc95148c724ae53947f83
Mayura Nawarathne <mayuraviraj@gmail.com>
2023-05-24 13:15:06 +0200

    Update TopMostK.java

--- java/TopMostK.java
+++ java/TopMostK.java
@@ -1,10 +1,10 @@
-// Find top k frequent
-// Create freq map
-// sort the values desc
-// get top most k
-// sort nlogn + on traverse which result in ologn.
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    // Find top k frequent
+    // Create freq map
+    // sort the values desc
+    // get top most k
+    // sort nlogn + on traverse which result in ologn.
+    public int[] topKFrequentWithSortedList(int[] nums, int k) {
         Map<Integer,Integer> intFreq = new HashMap<>();
         for(int i : nums){
             intFreq.put(i, intFreq.getOrDefault(i,0) + 1);
@@ -15,6 +15,23 @@ class Solution {
         int[] array = new int[k];
         for(int i = 0; i < k; i++){
             array[i] = sorted.get(i).getKey();
+        }
+        return array;
+    }
+
+    // Instead of sort we can use maxheap as it would cost only log n create poll.
+    // Max heap is possilbe because we need top most priority ( in this case max values) values
+    public int[] topKFrequentWithMaxHeap(int[] nums, int k) {
+        Map<Integer, Integer> intFreq = new HashMap<>();
+        for (int i : nums) {
+            intFreq.put(i, intFreq.getOrDefault(i, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
+        queue.addAll(intFreq.entrySet());
+        
+        int[] array = new int[k];
+        for (int i = 0; i < k; i++) {
+            array[i] = queue.poll().getKey();
         }
         return array;
     }
